7bf9d2ebd04a970b0c2a8f5fe3a38690d0bf879f
Jamie Orr <Jamie.Orr@newday.co.uk>
2024-05-29 14:50:10 +0100

    romans

--- src/index.ts
+++ src/index.ts
@@ -32,4 +32,80 @@ export function toJadenCase(sentence: string): string {
     let result = sentence.split(" ").map(word => word[0].toString().toUpperCase() + (word.slice(1))).join(" ")
     return result
 }
-  
+ 
+// CodeWars: Roman Numerals Decoder
+export function romanNumeralsDecoder(numeral: string): number {
+    const numeralLetters = ["I", "V", "X", "L", "C", "D", "M"]
+    const numeralNumbers = [1, 5, 10, 50, 100, 500, 1000]
+    
+    let currentComposite = ""
+    let currentCompositeTotal = 0
+    let currentTotal = 0
+    
+    for (let i = 0; i < numeral.length; i++) {
+        let currentNumeral = numeral[i]
+        let numeralIndex = numeralLetters.indexOf(currentNumeral)
+        let numeralNumber = numeralNumbers[numeralIndex]
+        
+        if (currentComposite == "") {
+            currentComposite = currentNumeral
+            currentCompositeTotal += numeralNumber
+        }
+        else if (numeralNumber <= currentCompositeTotal && currentNumeral == currentComposite) {
+            currentCompositeTotal += numeralNumber
+        }
+        else if (numeralNumber > currentCompositeTotal) {
+            currentTotal += (numeralNumber - currentCompositeTotal)
+            currentComposite = ""
+            currentCompositeTotal = 0
+        }
+        else {
+            currentTotal += currentCompositeTotal
+            currentComposite = currentNumeral
+            currentCompositeTotal = numeralNumber
+        }
+    }
+    currentTotal += currentCompositeTotal
+    
+    return currentTotal
+}
+
+// CodeWars: Roman Numerals Encoder
+export function romanNumeralsEncoder(number: number): string {
+    const numerals: Record<number, string> = {
+        1: "I",
+        5: "V",
+        10: "X",
+        50: "L",
+        100: "C",
+        500: "D",
+        1000: "M",
+    }
+    
+    let numberSplit = number.toString().split('').reverse()
+    let resultArray = []
+
+    for (let i = 0; i < numberSplit.length; i++) {
+        let currentNumber = parseInt(numberSplit[i])
+        let currentSize = 10 ** i
+        if (currentNumber < 4) {
+            let currentResult = numerals[currentSize].repeat(currentNumber) 
+            resultArray.push(currentResult)
+        }
+        else if (currentNumber == 4) {
+            let currentResult = numerals[currentSize] + numerals[currentSize * 5] 
+            resultArray.push(currentResult)
+        }
+        else if (currentNumber >= 5 && currentNumber < 9) {
+            let currentResult = numerals[currentSize * 5] + (numerals[currentSize].repeat(currentNumber - 5))
+            resultArray.push(currentResult)
+        }
+        else {
+            let currentResult = numerals[currentSize] + numerals[currentSize * 10]
+            resultArray.push(currentResult)
+        }
+    }
+    
+    let result = resultArray.reverse().join('') 
+    return result
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { add, reverseFiveLetterWords, toJadenCase } from '../src/index';
+import { add, reverseFiveLetterWords, romanNumeralsDecoder, romanNumeralsEncoder, toJadenCase } from '../src/index';
 
 describe('Example: Add numbers', () => {
   test('empty string should result in zero', () => {
@@ -41,3 +41,42 @@ describe('CodeWars: Jaden Casing Strings', () => {
     expect(toJadenCase('jamie orr')).toBe('Jamie Orr');
   });
 });
+
+describe('CodeWars: Roman Numerals Decoder', () => {
+  test('Single numeral, I', () => {
+    expect(romanNumeralsDecoder('I')).toBe(1);
+  });
+  test('Single numeral, V', () => {
+    expect(romanNumeralsDecoder('V')).toBe(5);
+  });
+  test('Simple multiple numeral, VI', () => {
+    expect(romanNumeralsDecoder('VI')).toBe(6);
+  });
+  test('Simple multiple numeral, MM', () => {
+    expect(romanNumeralsDecoder('MM')).toBe(2000);
+  });
+  test('Composite multiple numeral, IV', () => {
+    expect(romanNumeralsDecoder('IV')).toBe(4);
+  });
+  test('Composite multiple numeral, XIV', () => {
+    expect(romanNumeralsDecoder('XIV')).toBe(14);
+  });
+  test('Composite multiple numeral, MDCLXVI', () => {
+    expect(romanNumeralsDecoder('MDCLXVI')).toBe(1666);
+  });
+});
+
+describe('CodeWars: Roman Numerals Encoder', () => {
+  test('Single letter, I', () => {
+    expect(romanNumeralsEncoder(1)).toBe("I")
+  })
+  test('Simple multiple letters, II', () => {
+    expect(romanNumeralsEncoder(2)).toBe("II")
+  })
+  test('Composite multiple letters, IV', () => {
+    expect(romanNumeralsEncoder(4)).toBe("IV")
+  })
+  test('Composite multiple letters, MMMCMXCIX', () => {
+    expect(romanNumeralsEncoder(3999)).toBe("MMMCMXCIX")
+  })
+})
